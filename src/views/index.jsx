fa7c38e7eb33d569279b07b9a4fbfb3cc4d32a15
wvit <1999wuwei@gmail.com>
2019-05-09 18:35:44 +0800

    null

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import HeadTitle from '../components/public/headTitle'
-import ColorPicker from 'colorPicker'
+import { ColorPicker } from 'moha'
 
-console.log(ColorPicker)
 class Index extends React.Component {
     render() {
         return (
             <div>
                 <HeadTitle titleName="首页" />
-                {/* <ColorPicker /> */}
+                <ColorPicker className="ColorPicker" type='BlockPicker' />
             </div>
         )
     }
