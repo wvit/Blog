372b6d8b78d7042811bdc2eeba213d282066d64a
wvit <1999wuwei@gmail.com>
2019-05-09 21:42:57 +0800

    null

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import HeadTitle from '../components/public/headTitle'
-import { ColorPicker } from 'moha'
+import { ColorPicker } from 'moha-ui'
 
 class Index extends React.Component {
-    render() {
+    render () {
         return (
             <div>
                 <HeadTitle titleName="首页" />
