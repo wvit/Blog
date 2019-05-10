f6d5809a0c8a531c5c28e83d8eef71a297df56ec
wvit <1999wuwei@gmail.com>
2019-05-10 15:54:27 +0800

    null

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import HeadTitle from '../components/public/headTitle'
-import { ColorPicker } from 'moha-ui'
 
 class Index extends React.Component {
-    render () {
+    render() {
         return (
             <div>
                 <HeadTitle titleName="首页" />
-                <ColorPicker className="ColorPicker" type='BlockPicker' />
             </div>
         )
     }
--- src/views/user.jsx
+++ src/views/user.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
 import HeadTitle from '../components/public/headTitle'
-import ColorPicker from '../components/user/colorPicker'
+import { ColorPicker } from 'moha-ui'
 import '../assets/css/user/user.css'
 
 class User extends React.Component {
-    render() {
-        return (
-            <div className="user-wrap">
-                <HeadTitle titleName="我的" />
-                <ul className="user-list">
-                    <li className="clearfix icon icon-colorPicker">
-                        <span >
-                            选择主题：
-                        </span>
-                        <div>
-                            <ColorPicker className="ColorPicker" type='BlockPicker' />
-                        </div>
-                    </li>
-                </ul>
-            </div >
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      //颜色值
+      colorValue: 'blue'
     }
+  }
+  render() {
+    return (
+      <div className="user-wrap">
+        <HeadTitle titleName="我的" />
+        <ul className="user-list">
+          <li className="clearfix icon icon-colorPicker">
+            <span >选择主题：</span>
+            <div>
+              <ColorPicker
+                className="ColorPicker" type='BlockPicker'
+                color={this.state.colorValue}
+                colorChange={this.colorChange.bind(this)}
+              />
+            </div>
+          </li>
+        </ul>
+      </div >
+    )
+  }
+  //颜色值变化
+  colorChange(ev) {
+    this.setState({
+      colorValue: ev.hex
+    })
+  }
 }
 
 export default User
