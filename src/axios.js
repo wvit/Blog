6bb61054c8bb53f695ee6eed8d40d4dac3a61631
wvit <1999wuwei@gmail.com>
2019-05-27 19:38:48 +0800

    前端展示blog

--- src/axios.js
+++ src/axios.js
@@ -1,18 +1,6 @@
 import axios from 'axios'
-import {
-  networkInterfaces
-} from 'os';
 const production = false;
-let address = '';
-
-for (let devName in networkInterfaces()) {
-  interfaces[devName].forEach(item => {
-    if (item.family === 'IPv4' && item.address !== '127.0.0.1') {
-      address = item.address;
-    }
-  })
-}
-const url = production ? '132.232.114.236' : address
+const url = production ? '132.232.114.236' : '10.100.115.103'
 
 export default axios.create({
   baseURL: `http://${url}:1999`
--- src/views/learn.jsx
+++ src/views/learn.jsx
@@ -1,26 +1,33 @@
 import React from 'react'
 import { Title } from 'moha-ui'
-import a from '../assets/img/a.png'
+import axios from '../axios'
 import '../assets/css/learn/learn.css'
 
 class Learn extends React.Component {
-  render () {
+  constructor(props) {
+    super(props)
+    this.getBlogList();
+    this.state = {
+      blogList: []//博客列表
+    }
+  }
+  render() {
     return (
       <div>
         <Title titleName='学习日志' className="mainBgColor" />
         <ul className="blog-list">
           {
-            [1, 2, 3].map((item, index) => {
+            this.state.blogList.map((item, index) => {
               return <li key={index}>
                 <div className="item-head">
-                  <h4>webpack踩坑{item}</h4>
-                  <p><i className="icon icon-shijian"></i> 2019-5-11</p>
+                  <h4>{item.title}</h4>
+                  <p><i className="icon icon-shijian"></i> {item.addTime}</p>
                 </div>
                 <div className="item-content clearfix">
-                  <div className="text">
-                    webpack踩坑 webpack踩坑 webpack踩坑 webpack踩坑 webpack踩坑
-                        </div>
-                  <img src={a} alt="" className="img" />
+                  <div className="text" dangerouslySetInnerHTML={{ __html: item.content }}></div>
+                  {
+                    item.img ? <img src={item.img} className="img" alt="" /> : ''
+                  }
                 </div>
               </li>
             })
@@ -28,6 +35,22 @@ class Learn extends React.Component {
         </ul>
       </div>
     )
+  }
+  //获取blog列表
+  getBlogList() {
+    const blogList = [];
+    axios.get(`/app/getBlogs?key=&page=1&pageSize=10`).then(res => {
+      if (res.data.code !== 0) return;
+      res.data.data.list.forEach(item => {
+        const content = new DOMParser().parseFromString(item.content, 'text/html');
+        const img = content.querySelector('img');
+        if (img) item.img = img.src;
+        blogList.push(item)
+      });
+      this.setState({
+        blogList
+      });
+    });
   }
 }
 
